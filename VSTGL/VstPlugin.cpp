00c646d246bfb98fd27b80cc517b50ab6522351e
Jin <jinjuyu@naver.com>
2012-11-08 06:37:55 +0900

    I think it's at least not Echo... but not sure
    since crashing is so random

--- VSTGL/VstPlugin.cpp
+++ VSTGL/VstPlugin.cpp
@@ -88,9 +88,10 @@ vendorName("ndc Plugs")
 	strcpy(DATADIR, b.c_str());
 
 	// RAKARRACK effects
+	
 	mEffEcho = new Echo(nullptr, nullptr);
 	//mEffDistorsion = new Distorsion(nullptr, nullptr);
-	mEffConvolotron = new Convolotron(nullptr, nullptr, 1, 4, 2);
+	//mEffConvolotron = new Convolotron(nullptr, nullptr, 1, 4, 2);
 
 	//presets
 	
@@ -99,12 +100,12 @@ vendorName("ndc Plugs")
         mEffEcho->changepar (n, preset[n]);
 	int preset2[11] =  {84, 64, 35, 56, 40, 0, 0, 6703, 21, 0, 0};
     for (int n = 0; n < 11; n++)
-      ;//  mEffDistorsion->changepar (n, preset2[n]);
+        ;//mEffDistorsion->changepar (n, preset2[n]);
 	
 	int preset3[11] = {67, 64, 1, 100, 0, 64, 30, 20, 0, 0, 0};
     for (int n = 0; n < 11; n++)
-        mEffConvolotron->changepar (n, preset3[n]);
-
+        ;//mEffConvolotron->changepar (n, preset3[n]);
+		
 	// originals
 	int i;
 
@@ -153,7 +154,7 @@ VstPlugin::~VstPlugin()
 {
 	delete mEffEcho;
 	//delete mEffDistorsion;
-	delete mEffConvolotron;
+	//delete mEffConvolotron;
 	int i;
 
 	//Delete event queue.
@@ -217,7 +218,7 @@ void VstPlugin::processReplacing(float **inputs,
 	}
 	//mEffDistorsion->processReplacing(inputs, outputs, sampleFrames);
 	//mEffConvolotron->processReplacing(inputs, outputs, sampleFrames);
-	//mEffEcho->processReplacing(inputs, outputs, sampleFrames);
+	mEffEcho->processReplacing(inputs, outputs, sampleFrames);
 	//If there are events remaining in the queue, update their delta values.
 	if(numPendingEvents > 0)
 	{
