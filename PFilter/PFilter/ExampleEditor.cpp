3090acb4819c5c2d65c61be131b9e74ac2c87677
Jin <jinjuyu@naver.com>
2012-12-12 22:37:57 +0900

    a
    a

---
+++ PFilter/PFilter/ExampleEditor.cpp
@@ -0,0 +1,130 @@
+//	ExampleEditor.cpp - Simple OpenGL editor window.
+//	--------------------------------------------------------------------------
+//	Copyright (c) 2005-2006 Niall Moody
+//	
+//	Permission is hereby granted, free of charge, to any person obtaining a
+//	copy of this software and associated documentation files (the "Software"),
+//	to deal in the Software without restriction, including without limitation
+//	the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//	and/or sell copies of the Software, and to permit persons to whom the
+//	Software is furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//	THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//	DEALINGS IN THE SOFTWARE.
+//	--------------------------------------------------------------------------
+
+#include "ExampleEditor.h"
+
+//----------------------------------------------------------------------------
+ExampleEditor::ExampleEditor(AudioEffect *effect):
+VSTGLEditor(effect, Antialias4x),
+Timer(30), //30ms == ~30fps?
+thing(0.0f)
+{
+	//Set the opengl context's size - This must be called here!
+	setRect(0, 0, 400, 300);
+}
+
+//----------------------------------------------------------------------------
+ExampleEditor::~ExampleEditor()
+{
+	
+}
+
+//----------------------------------------------------------------------------
+void ExampleEditor::guiOpen()
+{
+	//Setup OpenGL stuff.
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+	glViewport(0, 0, getWidth(), getHeight());
+
+    //Set viewing projection.
+    glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	gluPerspective(45.0f,
+				   (GLfloat)getWidth()/(GLfloat)getHeight(),
+				   0.1f,
+				   100.0f);
+
+    //Position viewer.
+    glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+    glEnable(GL_DEPTH_TEST);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_LINE_SMOOTH);
+	glLineWidth(2.0f);
+
+	//Start timer to constantly update gui.
+	start();
+}
+
+//----------------------------------------------------------------------------
+void ExampleEditor::guiClose()
+{
+	//Stop the timer.
+	stop();
+}
+
+//----------------------------------------------------------------------------
+void ExampleEditor::draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glTranslatef(0.0f, 0.0f, -6.0f);
+	glRotatef(thing, 1.0f, 1.0f, 1.0f);
+	thing += 2.0f;
+
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f,0.0f,0.0f);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f,1.0f,0.0f);
+		glVertex3f(-1.0f,-1.0f, 1.0f);
+		glColor3f(0.0f,0.0f,1.0f);
+		glVertex3f( 1.0f,-1.0f, 1.0f);
+
+		glColor3f(1.0f,0.0f,0.0f);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f,0.0f,1.0f);
+		glVertex3f( 1.0f,-1.0f, 1.0f);
+		glColor3f(0.0f,1.0f,0.0f);
+		glVertex3f( 1.0f,-1.0f, -1.0f);
+
+		glColor3f(1.0f,0.0f,0.0f);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f,1.0f,0.0f);
+		glVertex3f( 1.0f,-1.0f, -1.0f);
+		glColor3f(0.0f,0.0f,1.0f);
+		glVertex3f(-1.0f,-1.0f, -1.0f);
+
+		glColor3f(1.0f,0.0f,0.0f);
+		glVertex3f( 0.0f, 1.0f, 0.0f);
+		glColor3f(0.0f,0.0f,1.0f);
+		glVertex3f(-1.0f,-1.0f,-1.0f);
+		glColor3f(0.0f,1.0f,0.0f);
+		glVertex3f(-1.0f,-1.0f, 1.0f);
+	glEnd();
+}
+
+//----------------------------------------------------------------------------
+void ExampleEditor::timerCallback()
+{
+	refreshGraphics();
+}
